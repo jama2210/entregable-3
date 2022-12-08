040b93f2ca9d4ed8a81f0002b093e5e3e57eb8fb
Jose_Maldonado <josemaldo2214@gmail.com>
2022-12-08 16:15:50 -0300

    react3

---
+++ src/App.jsx
@@ -0,0 +1,64 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import "./App.css";
+import ErrorFetch from "./components/ErrorFetch";
+import LocationInfo from "./components/LocationInfo";
+import ResidentCard from "./components/ResidentCard";
+
+function App() {
+  // https://rickandmortyapi.com/api/location/3
+  const [location, setLocation] = useState();
+  const [locationInput, setLocationInput] = useState();
+  const [hasError, setHasError] = useState(false);
+
+  useEffect(() => {
+    let URL;
+
+    if (locationInput) {
+      URL = `https://rickandmortyapi.com/api/location/${locationInput}`;
+    } else {
+      const randomIdLocation = Math.floor(Math.random() * 126) + 1;
+      URL = `https://rickandmortyapi.com/api/location/${randomIdLocation}`;
+    }
+
+    axios
+      .get(URL)
+      .then((res) => {
+        setLocation(res.data);
+        setHasError(false);
+      })
+      .catch((err) => {
+        setHasError(true);
+        console.log(err);
+      });
+  }, [locationInput]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setLocationInput(e.target.inputSearch.value);
+  };
+
+  return (
+    <div className="App">
+      <h1>Rick and Morty</h1>
+      <form onSubmit={handleSubmit}>
+        <input id="inputSearch" type="text" />
+        <button>Search</button>
+      </form>
+      {hasError ? (
+        <ErrorFetch />
+      ) : (
+        <>
+          <LocationInfo location={location} />
+          <div className="residents-container">
+            {location?.residents.map((url) => (
+              <ResidentCard key={url} url={url} />
+            ))}
+          </div>
+        </>
+      )}
+    </div>
+  );
+}
+
+export default App;
